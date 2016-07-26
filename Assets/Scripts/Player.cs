a796b68b608d31a23c656bba17fa48baedf5a094
skr47ch_g4m3z <its.me.arrived@gmail.com>
2016-07-26 23:36:17 +0530

    Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (PlayerController))]
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 4.0f;
+
+	PlayerController controller;
+	Camera viewCamera;
+
+	void Start () {
+		viewCamera = Camera.main;
+		controller = GetComponent<PlayerController>();
+	}
+
+	void Update () {
+		// move input
+		if (Input.GetMouseButton(0)) {
+
+			Vector2 targetPos = viewCamera.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 moveVelocity = targetPos.normalized * moveSpeed;
+			Debug.Log ("Cliceked");
+			controller.Move (targetPos, moveSpeed);
+		} 
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Vector2 target;
+	float speed = 2;
+	bool moveFlag = true;
+
+	void Start () {
+		target = transform.position;
+	}
+
+	void Update () {
+		Debug.Log (moveFlag);
+
+		if ((Vector2) transform.position != target) {
+			moveFlag = false;
+			transform.position = Vector2.MoveTowards (transform.position, target, speed * Time.deltaTime);
+		}
+		else moveFlag = true;
+	}
+
+	public void Move (Vector2 targetPosition_player, float moveSpeed_player) {
+		if (moveFlag) {
+			target = targetPosition_player;
+			speed = moveSpeed_player;
+		}
+	}
+}
